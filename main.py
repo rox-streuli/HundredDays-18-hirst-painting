66688f536ef2f99bc8151ad27e9a8f5705601d62
rox-streuli <roxstreuli@gmail.com>
2023-12-22 16:39:11 +0000

    Create Turtle object named dot. Set shape of dot to 'circle'. Add import turtle, Turtle, Screen and random.choice Create Screen object. Set to exitonclick()

--- main.py
+++ main.py
@@ -7,6 +7,10 @@
 # for item in colours:
 #     rgb_colour_list.append((item.rgb.r, item.rgb.g, item.rgb.b))
 # print(rgb_colour_list)
+import turtle
+from turtle import Turtle, Screen
+from random import choice
+
 colour_list = [(17, 22, 51), (156, 72, 49), (199, 149, 115), (45, 26, 15),
                (56, 17, 28), (235, 228, 94), (133, 164, 199), (135, 25, 38),
                (145, 71, 88), (73, 88, 127), (188, 143, 157), (189, 86, 120),
@@ -14,3 +18,11 @@ colour_list = [(17, 22, 51), (156, 72, 49), (199, 149, 115), (45, 26, 15),
                (97, 110, 183), (34, 46, 127), (191, 97, 79), (225, 172, 185),
                (191, 128, 52), (171, 185, 224), (231, 173, 164), (47, 83, 42),
                (167, 207, 198), (165, 204, 211)]
+
+dot = Turtle()
+turtle.colormode(255)
+
+dot.shape('circle')
+
+screen = Screen()
+screen.exitonclick()
